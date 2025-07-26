080e97deacf7fe00a15a1c1d3d9c14e085ad6387
Jake Black <jhilboldt@gmail.com>
2025-07-26 11:46:23 +0000

    The head is oval, the glasses are complete circles, a single note, where

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,17 +15,17 @@ const Logo = () => (
     strokeLinejoin="round"
     className="text-primary"
   >
-    {/* Head outline */}
-    <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 14.3443 2.76654 16.4849 4.07437 18.1743" />
+    {/* Oval Head */}
+    <ellipse cx="12" cy="12" rx="9" ry="11" />
+    
+    {/* Circle Glasses */}
+    <circle cx="8.5" cy="12" r="2.5" />
+    <circle cx="15.5" cy="12" r="2.5" />
+    <line x1="11" y1="12" x2="13" y2="12" />
 
-    {/* Glasses */}
-    <circle cx="8.5" cy="11" r="2" />
-    <circle cx="15.5" cy="11" r="2" />
-    <line x1="10.5" y1="11" x2="13.5" y2="11" />
-
-    {/* Music Note in head */}
-    <path d="M12 9a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0-2v4m2-2h-4" transform="translate(1, -0.5) scale(0.8)" />
-    <path d="M14,10 L14,7 C14,6.44771525 13.5522847,6 13,6 L11,6 C10.4477153,6 10,6.44771525 10,7 L10,8.5 C10,9.88071187 11.1192881,11 12.5,11 L12.5,11 C13.8807119,11 15,9.88071187 15,8.5 Z" strokeWidth="1" />
+    {/* Single Music Note */}
+    <path d="M12 10.5a2 2 0 1 0 -4 0v3.5h4" />
+    <path d="M12 8a2 2 0 1 1 4 0v3.5h-4" />
   </svg>
 );
 
