10d099ce13ae0390c51a8f4ab5ee2be3975b4699
Jake Black <jhilboldt@gmail.com>
2025-08-05 16:44:53 +0000

    im sorry you mis understood me so revert back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
           <div className="flex flex-col min-h-screen">
             <Header />
+            {children}
           </div>
           <Toaster />
       </body>
