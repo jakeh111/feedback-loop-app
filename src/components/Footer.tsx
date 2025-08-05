1e165971ac19820dce3c08d29c438861ee470fc8
Jake Black <jhilboldt@gmail.com>
2025-08-05 17:28:59 +0000

    look at checkpoint 6a3e04f, this had the correct positioning for the bee

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,7 +19,7 @@ export function Footer() {
                     </div>
                     <div className="flex flex-col gap-2 items-start md:items-end">
                         <Button asChild>
-                            <Link href="#" className="group gap-0">
+                            <Link href="#" className="group">
                                 <LinkIcon />
                                 <div className="max-w-[60px] group-hover:max-w-0 overflow-hidden transition-all duration-300 ease-in-out ml-2">
                                   Socials
